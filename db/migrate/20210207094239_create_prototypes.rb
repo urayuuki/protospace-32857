998bac0b0a85e893de30fdfff84ff5c62acd7287
urayuuki <76430661+urayuuki@users.noreply.github.com>
2021-02-23 18:33:48 +0900

    first_commit

---
+++ db/migrate/20210207094239_create_prototypes.rb
@@ -0,0 +1,15 @@
+class CreatePrototypes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :prototypes do |t|
+
+      t.string :title  
+      t.text   :catch_copy
+      t.text   :concept 
+      t.references :user, foreign_key: true
+      
+  
+
+      t.timestamps
+    end
+  end
+end
